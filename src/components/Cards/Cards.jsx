30e225aaa715750f97d243dda70a7b594767d93e
nawfal4756 <nawfal4756@users.noreply.github.com>
2020-06-23 20:15:58 +0500

    First Basic Commit

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Cards.module.css'
+
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+
+export const Cards = (props) => {
+    console.log(props);
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+}
